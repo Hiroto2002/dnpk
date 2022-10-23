3ca8f641bb805e5ce304e49098aef2158042a9bb
hiroto2002 <hiroto2002h14@icloud.com>
2022-10-23 22:41:25 +0900

    調整

--- index.php
+++ index.php
@@ -30,8 +30,6 @@ if (isset($_GET["logout"])) {
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>どんぷく OES ホーム画面</title>
     <link rel="stylesheet" href="css/style.css">
     <!-- <meta name="apple-mobile-web-app-capable" content="yes">
--- order.php
+++ order.php
@@ -62,8 +62,6 @@ if (isset($_POST["odh_No"])) {
 
 <head>
   <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>注文画面</title>
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/order.css">
