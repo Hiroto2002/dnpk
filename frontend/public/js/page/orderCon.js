5b7b8589c317726ba41c197736a38da571287ad9
Hiroto2002 <hiroto2002h14@icloud.com>
2025-02-18 16:57:15 +0900

    fix:db

--- frontend/public/js/page/orderCon.js
+++ frontend/public/js/page/orderCon.js
@@ -1,4 +1,88 @@
 "use strict";
-const test = () => {
-    console.log("orderCon");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
+/* ピッチインピッチアウトによる拡大縮小を禁止 */
+document.documentElement.addEventListener("touchstart", function (e) {
+    if (e.touches.length >= 2) {
+        e.preventDefault();
+    }
+}, {
+    passive: false,
+});
+/* ダブルタップによる拡大を禁止 */
+var t = 0;
+document.documentElement.addEventListener("touchend", function (e) {
+    var now = new Date().getTime();
+    if (now - t < 350) {
+        e.preventDefault();
+    }
+    t = now;
+}, false);
+setInterval(() => {
+    location.reload();
+}, 60000);
+function MoveCheck($str) {
+    var res = confirm("オーダー番号" + $str + "を削除しますか？");
+    if (res == true) {
+        // 再度確認
+        var res = confirm("本当に削除しますか？");
+        if (res == true) {
+            //OKなら削除処理に進む
+        }
+        else {
+            // キャンセルなら処理を中断する
+            return false;
+        }
+    }
+    else {
+        // キャンセルなら処理を中断する
+        return false;
+    }
+}
+$(function () {
+    $(".reload").on("click", function () {
+        location.reload();
+    });
+});
+$(function () {
+    $(".widget-list-link2").on("click", function (e) {
+        return __awaiter(this, void 0, void 0, function* () {
+            e.preventDefault();
+            const odhNo = $(this).attr("data-id");
+            const situNo = $(this).attr("id");
+            const url = $(this).attr("href");
+            if (odhNo === undefined || situNo === undefined || url === undefined) {
+                alert("エラーが発生しました");
+                return;
+            }
+            yield fetch(`./api/checkSituation.php?odhNo=${odhNo}&situNo=${situNo}`)
+                .then((res) => {
+                if (!res.ok) {
+                    throw new Error("サーバーエラーが発生しました");
+                }
+                return res.json();
+            })
+                .then((data) => {
+                if (data === false) {
+                    alert("ぐるぐるで更新してください");
+                    location.reload();
+                }
+                else {
+                    location.href = url;
+                }
+                // 他のデータ処理をここに追加
+            })
+                .catch((error) => {
+                console.error("エラー:", error);
+                alert("エラーが発生しました");
+            });
+        });
+    });
+});
